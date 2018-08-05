9b22ac1b85b0c9a4a96938b104145dd6eabbff6b
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-05 23:14:55 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(array)
   squared = []
-  array.each {|element| squared.push(element**2)}
-  puts "#{squared}".to_a
+  array.each {|element| squared << element**2}
+  puts "#{squared}"
 end
